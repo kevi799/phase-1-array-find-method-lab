27ff170b837d194a4afdcfd4be61aa25911e2402
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-10-07 09:10:05 +0300

    Adds the find method

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const record = [
+  { year: "1998", result: "w" },
+  { year: "1994", result: "N/A" },
+  { year: "1987", result: "L" },
+];
+
+// function to single a result in the game record data
+function superbowlWin(record) {
+  function timesWon(bowling) {
+    if (bowling.result === "W") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  //using the find method to look for the time he first won
+  const recordWins = record.find(timesWon)
+  // if statement to return the year won 
+  if (recordWins) {
+    return recordWins.year;
+  }
+  else{
+    return undefined;
+  }
+
+}
